eaf3bb46a8484ab6c3c08296948d74d47aa9a4d9
Artsiom_Reutovich <artsiom_reutovich@epam.com>
2022-10-03 12:53:12 +0500

    added router configuration

---
+++ pages/items/index.tsx
@@ -0,0 +1,9 @@
+export default function ItemsPage() {
+    return (
+        <ul>
+            <li>TVs</li>
+            <li>Laptops</li>
+            <li>Mobile phones</li>
+        </ul>
+    )
+}
