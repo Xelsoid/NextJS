464bbcedef4c754a3d5fbdd4ce4fdc46e8f0ac96
Artsiom_Reutovich <artsiom_reutovich@epam.com>
2022-10-04 11:48:53 +0500

    added css modules

--- components/Typography.tsx
+++ components/Typography.tsx
@@ -1,9 +1,10 @@
 import { FunctionComponent } from "react";
+import styles from './Typography.module.css'
 
 interface Props {
     text: string;
 }
 
 export const Typography: FunctionComponent<Props> = ({text}) => (
-    <span>{text}</span>
+    <span className={styles.typography}>{text}</span>
 );
